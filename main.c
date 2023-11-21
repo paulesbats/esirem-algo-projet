23705bb915cd474284216a951fa318fad73f9e23
TetevuideYanis <tetevuide.yanis@gmail.com>
2023-11-21 14:29:54 +0100

    1.3 - deck struct

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+//struct which can contain a undefined number of card
+typedef struct{
+    card card;
+    card *next;
+} Deck;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
