5a6b6454fd8367717cfb2167b01177ed28b18c10
Paulo <paullesbats2003@gmail.com>
2023-11-21 14:49:30 +0100

    Création enum Color

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+enum Color{SPADE, HEART, DIAMOND, CLOVER};
+
+typedef struct card{ // structure pour chaque carte
+    int valeur;
+    enum Color color;
+} card;
+
+
+//struct which can contain a undefined number of card
+typedef struct Deck{
+    card card;
+    card *next;
+} Deck;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
