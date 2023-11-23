a5f1c21b3b1727ce144a6b43e8c0b3626ca51e22
Paulo <paullesbats2003@gmail.com>
2023-11-23 08:23:27 +0100

    Fonction ShowHand Avec modification de pointeurs

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 //Debut du projet
 
 typedef enum Color{SPADE, HEART, DIAMOND, CLOVER}Color;
@@ -11,22 +13,42 @@ typedef struct Card{ // structure pour chaque carte
 //struct which can contain a undefined number of card
 typedef struct Deck{
     Card card;
-    Card *next;
-} Deck;
-
+    struct Deck *next;
+}Deck;
 
 typedef enum Choice{HIT,STAND,DOUBLE,SURREND}Choice;
 
 //struct which contain a deck and a value
 typedef struct Player {
-    Deck deck;
+    Deck *deck;
     int value;
 } Player;
 
-void ShowHand(){
+
+void ShowHand(Player player){
+    Deck *current = player.deck;
+    while(current != NULL){
+        int value = current->card.valeur;
+        Color color = current->card.color;
+        printf("%d de ", value);
+        switch (color) {
+            case (HEART):
+                printf("Coeur");
+                break;
+            case (SPADE):
+                printf("Pique");
+                break;
+            case (DIAMOND):
+                printf("Carreau");
+                break;
+            case (CLOVER):
+                printf("Trèfle");
+                break;
+        }
+        current = current->next;
+    }
 };
 
 int main() {
-    printf("Hello, World!\n");
     return 0;
 }
